9556c82de48ad54352912a15371c49d3a0405096
Jim Lawton <jim.lawton@gmail.com>
2017-04-27 15:19:06 +0100

    Watch config dir and auto-reload config on change.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -49,3 +49,18 @@ end
 
 -- Bind the Hyper key
 f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
+
+-- Reload config when any lua file in config directory changes, to save having to manually reload.
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == '.lua' then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+local myWatcher = hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reloadConfig):start()
+hs.alert.show('Config loaded')
