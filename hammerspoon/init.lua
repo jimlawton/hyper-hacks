7675ea2b3d26e3a7b27e39665503dbe9ca5c6416
Jim Lawton <jim.lawton@gmail.com>
2017-04-27 15:26:37 +0100

    Comment out stuff I don't use.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -20,34 +20,34 @@ for i, app in ipairs(singleapps) do
 end
 
 -- HYPER+G: Open www.google.com in the default browser
-lfun = function()
-  news = "app = Application.currentApplication(); app.includeStandardAdditions = true; app.doShellScript('open http://www.google.com')"
-  hs.osascript.javascript(news)
-  k.triggered = true
-end
-k:bind('', 'g', nil, lfun)
+-- lfun = function()
+--   news = "app = Application.currentApplication(); app.includeStandardAdditions = true; app.doShellScript('open http://www.google.com')"
+--   hs.osascript.javascript(news)
+--   k.triggered = true
+-- end
+-- k:bind('', 'g', nil, lfun)
 
 -- HYPER+M: Call a pre-defined trigger in Alfred 3
-mfun = function()
-  cmd = "tell application \"Alfred 3\" to run trigger \"emoj\" in workflow \"com.sindresorhus.emoj\" with argument \"\""
-  hs.osascript.applescript(cmd)
-  k.triggered = true
-end
-k:bind({}, 'm', nil, mfun)
+-- mfun = function()
+--   cmd = "tell application \"Alfred 3\" to run trigger \"emoj\" in workflow \"com.sindresorhus.emoj\" with argument \"\""
+--   hs.osascript.applescript(cmd)
+--   k.triggered = true
+-- end
+-- k:bind({}, 'm', nil, mfun)
 
 -- HYPER+E: Act like ⌃e and move to end of line.
-efun = function()
-  hs.eventtap.keyStroke({'⌃'}, 'e')
-  k.triggered = true
-end
-k:bind({}, 'e', nil, efun)
+-- efun = function()
+--   hs.eventtap.keyStroke({'⌃'}, 'e')
+--   k.triggered = true
+-- end
+-- k:bind({}, 'e', nil, efun)
 
 -- HYPER+A: Act like ⌃a and move to beginning of line.
-afun = function()
-  hs.eventtap.keyStroke({'⌃'}, 'a')
-  k.triggered = true
-end
-k:bind({}, 'a', nil, afun)
+-- afun = function()
+--   hs.eventtap.keyStroke({'⌃'}, 'a')
+--   k.triggered = true
+-- end
+-- k:bind({}, 'a', nil, afun)
 
 -- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
 pressedF18 = function()
