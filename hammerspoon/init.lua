ae3ac21dfb666fdaa8d6995f7d73c89a3f2efd5c
Jim Lawton <jim.lawton@gmail.com>
2025-11-06 13:55:20 +0000

    Reformat, fix lint issues.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,66 +1,63 @@
-
 -- A global variable for the Hyper Mode
-k = hs.hotkey.modal.new({}, "F17")
+local k = hs.hotkey.modal.new({}, "F17")
 
-launch = function(appname)
-  hs.application.launchOrFocus(appname)
-  k.triggered = trueo
+local launch = function(appname)
+    hs.application.launchOrFocus(appname)
+    k.triggered = true
 end
 
 -- Single keybinding for app launch
-singleapps = {
-  {'b', 'Brave Browser'},
-  {'f', 'Finder'},
-  {'g', 'Fork'},
-  {'o', 'Obsidian'},
-  {'m', 'Microsoft Outlook'},
-  {'s', 'Slack'},
-  {'y', 'YouTube Music Desktop App'},
-  {'z', 'Zed'}
+local singleapps = {
+    { 'b', 'Brave Browser' },
+    { 'f', 'Finder' },
+    { 'g', 'Fork' },
+    { 'o', 'Obsidian' },
+    { 'm', 'Microsoft Outlook' },
+    { 's', 'Slack' },
+    { 'y', 'YouTube Music Desktop App' },
+    { 'z', 'Zed' }
 }
-
 for i, app in ipairs(singleapps) do
-  k:bind({}, app[1], function() launch(app[2]); k:exit(); end)
+    k:bind({}, app[1], function()
+        launch(app[2]); k:exit();
+    end)
 end
 
 -- HYPER+D: Invoke Finder in Downloads folder.
-dfun = function()
-  hs.eventtap.keyStroke({'option', 'cmd'}, 'l')
-  k.triggered = true
+local finderDownloads = function()
+    hs.eventtap.keyStroke({ 'option', 'cmd' }, 'l')
+    k.triggered = true
 end
-k:bind({}, 'd', nil, dfun)
+k:bind({}, 'd', nil, finderDownloads)
 
 -- HYPER+TAB: OPTION+TAB
--- tabfun = function()
+-- local optionTab = function()
 --   hs.eventtap.keyStroke({'option'}, 'tab')
 --   k.triggered = true
 -- end
--- k:bind({}, 'tab', nil, tabfun)
+-- k:bind({}, 'tab', nil, optionTab)
 
 -- HYPER+H: Hide all windows.
-hfun = function()
-  hs.eventtap.keyStroke({'option', 'cmd'}, 'h')
-  hs.eventtap.keyStroke({'option', 'cmd'}, 'm')
-  k.triggered = true
+local hideAllWindows = function()
+    hs.eventtap.keyStroke({ 'option', 'cmd' }, 'h')
+    hs.eventtap.keyStroke({ 'option', 'cmd' }, 'm')
+    k.triggered = true
 end
-k:bind({}, 'h', nil, hfun)
+k:bind({}, 'h', nil, hideAllWindows)
 
 -- HYPER+T: Invoke iTerm2 visor.
--- tfun = function()
+-- local itermVisor = function()
 --   hs.eventtap.keyStroke({'fn', 'cmd'}, 'f12')
 --   k.triggered = true
 -- end
--- k:bind({}, 't', nil, tfun)
+-- k:bind({}, 't', nil, itermVisor)
 
--- HYPER+L: Lock screen
--- (This is now replaced by CMD-Ctrl-Q on High Sierra)
--- lfun = function()
-  -- This assumes that Keychain Access is in the menubar.
-  -- ascript = 'tell application "System Events" to tell process "SystemUIServer" to click (first menu item of menu 1 of ((click (first menu bar item whose description is "Keychain menu extra")) of menu bar 1) whose title is "Lock Screen")'
-  -- hs.osascript.applescript(ascript)
-  -- hs.osascript.applescript(ascript)
--- end
--- k:bind({}, 'l', nil, lfun)
+-- HYPER+L: Lock screen (original replaced by cmd-ctrl-q from High Sierra onwards)
+local lockSession = function()
+    hs.eventtap.keyStroke({ 'ctrl', 'cmd' }, 'q')
+    k.triggered = true
+end
+k:bind({}, 'l', nil, lockSession)
 
 -- HYPER+t: Open new iTerm2 window with default profile
 ifun = function()
@@ -74,27 +71,27 @@ end
 k.bind({}, "t", nil, ifun)
 
 -- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
-pressedF18 = function()
-  k.triggered = false
-  k:enter()
+local pressedF18 = function()
+    k.triggered = false
+    k:enter()
 end
 
 -- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
 --   send ESCAPE if no other keys are pressed.
-releasedF18 = function()
-  k:exit()
-  if not k.triggered then
-    hs.eventtap.keyStroke({}, 'ESCAPE')
-  end
+local releasedF18 = function()
+    k:exit()
+    if not k.triggered then
+        hs.eventtap.keyStroke({}, 'ESCAPE')
+    end
 end
 
 -- Bind the Hyper key
-f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
+local f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
 
 -- Reload config when any lua file in config directory changes, to save having to manually reload.
-function reloadConfig(files)
+local function reloadConfig(files)
     doReload = false
-    for _,file in pairs(files) do
+    for _, file in pairs(files) do
         if file:sub(-4) == '.lua' then
             doReload = true
         end
@@ -103,5 +100,8 @@ function reloadConfig(files)
         hs.reload()
     end
 end
-local myWatcher = hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reloadConfig):start()
+local myWatcher =
+    hs.pathwatcher.new(os.getenv('HOME') .. '/.local/share/chezmoi/hyper-hacks/.hammerspoon/init.lua', reloadConfig)
+    :start()
+
 hs.alert.show('Hammerspoon config reloaded')
